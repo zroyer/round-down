fc0cb85271534ed6dc9e83c2348b7242a5983041
zroyer <zroyer@utexas.edu>
2018-12-24 11:59:42 -0600

    implements standard boxing countdown

--- App.js
+++ App.js
@@ -1,11 +1,73 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button
+} from 'react-native';
+import TimerCountdown from 'react-native-timer-countdown';
+
+const INITIAL_ROUND_TIME = 180000;
+const INITIAL_BREAK_TIME = 30000;
 
 export default class App extends React.Component {
+  state = {
+    round: 1,
+    isBreak: false,
+    time: INITIAL_ROUND_TIME,
+  };
+
+  _roundOver() {
+    if (!this.state.isBreak) {
+      this.setState({
+        isBreak: true,
+        time: INITIAL_BREAK_TIME,
+      });
+    }
+    else {
+      this.setState({
+        round: this.state.round + 1,
+        isBreak: false,
+        time: INITIAL_ROUND_TIME,
+      });
+    }
+  }
+
+  _reset() {
+    this.setState({
+      round: 1,
+      isBreak: false,
+      time: INITIAL_ROUND_TIME,
+    });
+  }
+
   render() {
+    const {
+      round,
+      time,
+      isBreak,
+    } = this.state;
+
     return (
       <View style={styles.container}>
-        <Text>Round Down</Text>
+        <TimerCountdown
+          initialSecondsRemaining={time}
+          onTick={secondsRemaining => console.log("tick", secondsRemaining)}
+          onTimeElapsed={() => this._roundOver()}
+          allowFontScaling={true}
+          style={styles.timerCountdown}
+        />
+        <Text style={styles.roundIndicator}>
+          {isBreak
+            ? 'Break!'
+            : `Round ${round}`
+          }
+        </Text>
+        <Button
+          title='🔄'
+          onPress={() => this._reset()}
+          style={styles.resetButton}
+        />
       </View>
     );
   }
@@ -17,5 +79,14 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  timerCountdown: {
+    fontSize: 72,
+  },
+  roundIndicator: {
+    fontSize: 36,
+  },
+  resetButton: {
+    fontSize: 36,
   },
 });
